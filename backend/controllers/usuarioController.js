608a5a4e1f0a7fd37b90e71fd74cf0089eb0eee1
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-21 12:33:00 +1000

    Validando si el password es correcto y si el usuario existe

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -21,4 +21,33 @@ const registrar = async (req, res) => {
   }
 };
 
-export { registrar };
+const autenticar = async (req, res) => {
+  const { email, password } = req.body;
+
+  // Comprobar si el usuario existe
+  const usuario = await Usuario.findOne({ email });
+  if (!usuario) {
+    const error = new Error("El usuario no existe");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  // Comprobar si el usuario esta confirmado
+  if (!usuario.confirmado) {
+    const error = new Error("Tu cuenta no ha sido confirmada");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  // Comprobar su password
+  if (await usuario.comprobarPassword(password)) {
+    res.json({
+      _id: usuario._id,
+      nombre: usuario.nombre,
+      email: usuario.email,
+    });
+  } else {
+    const error = new Error("El password es incorrecto");
+    return res.status(403).json({ msg: error.message });
+  }
+};
+
+export { registrar, autenticar };
