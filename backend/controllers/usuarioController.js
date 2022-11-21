f9f21971384108c7bf4f0be93fff4459dd7f9650
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-21 12:39:34 +1000

    Creando el JWT

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,5 +1,6 @@
 import Usuario from "../models/Usuario.js";
 import generarId from "../helpers/generarId.js";
+import generarJWT from "../helpers/generarJWT.js";
 
 const registrar = async (req, res) => {
   // Evitar registros duplicados
@@ -43,6 +44,7 @@ const autenticar = async (req, res) => {
       _id: usuario._id,
       nombre: usuario.nombre,
       email: usuario.email,
+      token: generarJWT(usuario._id),
     });
   } else {
     const error = new Error("El password es incorrecto");
