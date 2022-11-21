f5c04555e423d54528da875257d885d5e2086d20
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-21 10:54:50 +1000

    Creando Routes, Controller y registrando usuarios

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import conectarDB from "./config/db.js";
+import usuarioRoutes from "./routes/usuarioRoutes.js";
 
 const app = express();
+app.use(express.json());
 
 dotenv.config();
 
 conectarDB();
+
+// Routing
+app.use("/api/usuarios", usuarioRoutes);
 
 const port = process.env.PORT || 4000;
 
