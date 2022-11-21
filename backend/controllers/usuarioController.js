ae41d11e0f75b1f1a59cecdf45397ad8d5ddeabb
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-21 12:02:19 +1000

    Generar Token unico

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,4 +1,5 @@
 import Usuario from "../models/Usuario.js";
+import generarId from "../helpers/generarId.js";
 
 const registrar = async (req, res) => {
   // Evitar registros duplicados
@@ -12,6 +13,7 @@ const registrar = async (req, res) => {
 
   try {
     const usuario = new Usuario(req.body);
+    usuario.token = generarId();
     const usuarioAlmacenado = await usuario.save();
     res.json(usuarioAlmacenado);
   } catch (error) {
