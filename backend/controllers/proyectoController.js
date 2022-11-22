ee22486e259dae8bff9b514c231848ea012a1134
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-22 18:11:09 +1000

    Obtener y Validar proyecto por el ID del creador

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -16,7 +16,23 @@ const nuevoProyecto = async (req, res) => {
     console.log(error);
   }
 };
-const obtenerProyecto = async (req, res) => {};
+const obtenerProyecto = async (req, res) => {
+  const { id } = req.params;
+
+  const proyecto = await Proyecto.findById(id);
+  if (!proyecto) {
+    const error = new Error("No encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Accion no valida");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  res.json(proyecto);
+};
+
 const editarProyecto = async (req, res) => {};
 const eliminarProyecto = async (req, res) => {};
 const agregarColaborador = async (req, res) => {};
