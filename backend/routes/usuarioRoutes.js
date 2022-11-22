be1768b784d9261ce503a5abf7653b3026484230
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-22 17:09:19 +1000

    Validando token y generando y almacenando nuevo password

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -5,6 +5,7 @@ import {
   confirmar,
   olvidePassword,
   comprobarToken,
+  nuevoPassword,
 } from "../controllers/usuarioController.js";
 
 const router = express.Router();
@@ -14,6 +15,6 @@ router.post("/", registrar);
 router.post("/login", autenticar);
 router.get("/confirmar/:token", confirmar);
 router.post("/olvide-password", olvidePassword);
-router.get("/olvide-password/:token", comprobarToken);
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
 
 export default router;
