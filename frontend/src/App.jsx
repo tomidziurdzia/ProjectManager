5d3fb93ad8e44760d5ff5e27e0d888843592e15a
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-24 08:35:31 +1000

    Creando el Routing con React Router Dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,26 @@
-function App() {
-  return <h1>hola</h1>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthLayout from "./layouts/AuthLayout";
+import ConfirmarCuenta from "./paginas/ConfirmarCuenta";
+import Login from "./paginas/Login";
+import NuevoPassword from "./paginas/NuevoPassword";
+import OlvidePassword from "./paginas/OlvidePassword";
+import Registrar from "./paginas/Registrar";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Area Publica */}
+        <Route path="/" element={<AuthLayout />}>
+          <Route index element={<Login />} />
+          <Route path="registrar" element={<Registrar />} />
+          <Route path="olvide-password" element={<OlvidePassword />} />
+          <Route path="olvide-password/:token" element={<NuevoPassword />} />
+          <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
