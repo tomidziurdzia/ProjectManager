920799b33783bd017d5f1a67bdeeb2dce1e811bc
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-24 11:02:58 +1000

    Configurando CORS y variables de entorno

--- frontend/src/paginas/Registrar.jsx
+++ frontend/src/paginas/Registrar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import Alerta from "../components/Alerta";
 
@@ -9,7 +10,7 @@ const Registrar = () => {
   const [repetirPassword, setRepetirPassword] = useState("");
   const [alerta, setAlerta] = useState({});
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     if ([nombre, email, password, repetirPassword].includes("")) {
@@ -25,6 +26,24 @@ const Registrar = () => {
     setAlerta({});
 
     // Crear el usuario en la API
+    try {
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/api/usuarios`,
+        {
+          nombre,
+          password,
+          email,
+        }
+      );
+      setAlerta({ msg: data.msg, error: false });
+
+      setNombre("");
+      setEmail("");
+      setPassword("");
+      setRepetirPassword("");
+    } catch (error) {
+      setAlerta({ msg: error.response.data.msg, error: true });
+    }
   };
 
   const { msg } = alerta;
