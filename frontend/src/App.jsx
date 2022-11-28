367c0c11c81bd64334bf25415a36768e3360fd3d
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-28 11:44:12 +1000

    Creando routing para editar proyecto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import RutaProtegida from "./layouts/RutaProtegida";
 import Proyectos from "./paginas/Proyectos";
 import NuevoProyecto from "./paginas/NuevoProyecto";
 import Proyecto from "./paginas/Proyecto";
+import EditarProyecto from "./paginas/EditarProyecto";
 
 import { AuthProvider } from "./context/AuthProvider";
 import { ProyectosProvider } from "./context/ProyectosProvider";
@@ -36,6 +37,7 @@ const App = () => {
               <Route index element={<Proyectos />} />
               <Route path="crear-proyecto" element={<NuevoProyecto />} />
               <Route path=":id" element={<Proyecto />} />
+              <Route path="editar/:id" element={<EditarProyecto />} />
             </Route>
           </Routes>
         </ProyectosProvider>
--- frontend/src/paginas/Proyecto.jsx
+++ frontend/src/paginas/Proyecto.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import useProyectos from "../hooks/useProyectos";
 
 const Proyecto = () => {
@@ -12,25 +12,30 @@ const Proyecto = () => {
 
   const { nombre } = proyecto;
 
-  return cargando ? (
-    <div className="border border-gray-400 shadow rounded-md p-4 w-1/2 mx-auto">
-      <div className="animate-pulse flex space-x-4">
-        <div className="rounded-full bg-slate-200 h-20 w-10"></div>
-        <div className="flex-1 space-y-6 py-1">
-          <div className="h-2 bg-slate-200 rounded"></div>
-          <div className="space-y-3">
-            <div className="grid grid-cols-3 gap-4">
-              <div className="h-2 bg-slate-200 rounded col-span-2"></div>
-              <div className="h-2 bg-slate-200 rounded col-span-1"></div>
-            </div>
-            <div className="h-2 bg-slate-200 rounded"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  ) : (
-    <div>
+  if (cargando) return "Cargando...";
+
+  return (
+    <div className="flex justify-between">
       <h1 className="font-black text-3xl">{nombre}</h1>
+      <div className="flex items-center gap-4 text-gray-400 hover:text-black hover:cursor-pointer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+          />
+        </svg>
+        <Link className="font-bold" to={`/proyectos/editar/${params.id}`}>
+          Editar
+        </Link>
+      </div>
     </div>
   );
 };
