f15dec2cde8375020640acf6b1eb8dfe6626f639
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-30 09:11:43 +1000

    Eliminar Colaborador y Sincronizar el State

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -145,7 +145,24 @@ const agregarColaborador = async (req, res) => {
   res.json({ msg: "Colaborador agregado correctamente", error: false });
 };
 
-const eliminarColaborador = async (req, res) => {};
+const eliminarColaborador = async (req, res) => {
+  const proyecto = await Proyecto.findById(req.params.id);
+
+  if (!proyecto) {
+    const error = new Error("Proyecto no encontrado");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Accion no valida");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  // Se puede eliminar
+  proyecto.colaboradores.pull(req.body.id);
+  await proyecto.save();
+  res.json({ msg: "Colaborador eliminado correctamente", error: false });
+};
 
 export {
   obtenerProyectos,
--- frontend/src/context/ProyectosProvider.jsx
+++ frontend/src/context/ProyectosProvider.jsx
@@ -359,7 +359,6 @@ const ProyectosProvider = ({ children }) => {
 
       setAlerta({ msg: data.msg, error: false });
       setColaborador({});
-      setAlerta({});
     } catch (error) {
       setAlerta({ msg: error.response.data.msg, error: true });
     }
@@ -371,7 +370,39 @@ const ProyectosProvider = ({ children }) => {
     setColaborador(colaborador);
   };
 
-  const eliminarColaborador = () => {};
+  const eliminarColaborador = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      if (!token) return;
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const { data } = await clienteAxios.post(
+        `/proyectos/eliminar-colaborador/${proyecto._id}`,
+        { id: colaborador._id },
+        config
+      );
+
+      const proyectoActualizado = { ...proyecto };
+      proyectoActualizado.colaboradores =
+        proyectoActualizado.colaboradores.filter(
+          (colaboradorState) => colaboradorState._id !== colaborador._id
+        );
+
+      setProyecto(proyectoActualizado);
+      setAlerta({ msg: data.msg, error: false });
+      setColaborador({});
+      setModalEliminarColaborador(false);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
 
   return (
     <ProyectosContext.Provider
