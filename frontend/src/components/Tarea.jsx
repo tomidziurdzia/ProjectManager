7d350e4712b50ce7e979d46ebe2dbea5aef24d09
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-29 12:09:34 +1000

    Mostrar las tareas en el royecto

---
+++ frontend/src/components/Tarea.jsx
@@ -0,0 +1,35 @@
+import { formatearFecha } from "../helpers/formatearFecha";
+
+const Tarea = ({ tarea }) => {
+  const { nombre, descripcion, prioridad, fechaEntrega, _id, estado } = tarea;
+  return (
+    <div className="border-b flex justify-between items-center px-2">
+      <div>
+        <p className="mb-2 text-xl">{nombre}</p>
+        <p className="mb-2 text-sm text-gray-500">{descripcion}</p>
+        <p className="mb-2 text-xl">{formatearFecha(fechaEntrega)}</p>
+        <p className="mb-2 text-xl text-gray-600">Prioridad: {prioridad}</p>
+      </div>
+      <div className="flex gap-2">
+        <button className="bg-indigo-500 px-4 py-3 text-white font-bold text-sm text-center rounded-lg">
+          Editar
+        </button>
+        {estado ? (
+          <button className="bg-sky-500 px-4 py-3 text-white font-bold text-sm text-center rounded-lg">
+            Completa
+          </button>
+        ) : (
+          <button className="bg-gray-500 px-4 py-3 text-white font-bold text-sm text-center rounded-lg">
+            Incompleta
+          </button>
+        )}
+
+        <button className="bg-red-500 px-4 py-3 text-white font-bold text-sm text-center rounded-lg">
+          Eliminar
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Tarea;
