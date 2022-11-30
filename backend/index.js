e37ff3e7407f7f2f80d1b410dde4f68a7822643a
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-30 17:14:20 +1000

    Anadir Socket.io a nuestro Servidor

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,22 @@ app.use("/api/tareas", tareaRoutes);
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
+const servidor = app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
+});
+
+// Socket.oio
+import { Server } from "socket.io";
+
+const io = new Server(servidor, {
+  pingTimeout: 60000,
+  cors: {
+    origin: process.env.FRONTEND_URL,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Conectado a socket.io");
+
+  // Definir los eventos
 });
